071024b998a9ee0743896a4c47e05f189b9ebad4
Iguta <davidigutas@students.uonbi.ac.ke>
2020-10-19 18:26:01 +0300

    more modularized, and error handling

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Product = require('../model/products');
+const multer = require('multer');
+const router = express.Router();
+const checkAuth = require('../middlewares/check-auth');
+
+const productsController = require('../controllers/products');
+
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename:(req, file, cb) => {
+        cb(null, `${new Date().toISOString().replace(/:/g,"")}${file.originalname}`);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    //accept file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    } else{
+        cb(new Error('message'), false);
+    };
+};
+const upload = multer({
+    storage:storage, 
+    limits:{
+    fileSize:1024 * 1024 *5
+    },
+    fileFilter:fileFilter
+});
+
+
+router.get('/', productsController.products_get_all);
+
+
+router.post('/', checkAuth, upload.single('productImg'), productsController.products_create_product);
+
+
+router.get('/:productId', productsController.products_get_product);
+
+router.patch('/:productId', checkAuth, productsController.products_update_product);
+
+router.delete('/:productId', checkAuth, productsController.products_delete_product);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-const express = require("express");
-require('dotenv').config();
+const http = require('http');
+const app = require('./app');
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,useUnifiedTopology: true , useCreateIndex: true})
-.then(()=> console.log("Connected to the database..."))
-.catch(err => console.log(err));
+const port = process.env.PORT || 3000;
 
-const app = express();
+const server = http.createServer(app);
 
-app.use(express.json());
-app.use('/uploads', express.static('uploads'));
-
-const productsRouter = require('./app/routes/products');
-app.use('/products', productsRouter);
-const PORT = process.env.PORT | 4000;
-const hostname = '127.0.0.1';
-
-
-app.get("/", (req, res) => {
-    res.send('Our front end home page goes here...');
-});
-app.listen(PORT, () => {
-    console.log(`Listening at ${hostname}:${PORT}`);
-})
-module.exports = {PORT, hostname};
+server.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
